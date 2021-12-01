2163471c4bea5f1cfb1b3e367d334fa4d56461b6
Akiinro <aktakinro@gmail.com>
2021-12-01 01:04:03 -0800

    commit

---
+++ posts/tests.py
@@ -0,0 +1,9 @@
+from _typeshed import Self
+from django import test
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+
+# Create your tests here.
+
+    
